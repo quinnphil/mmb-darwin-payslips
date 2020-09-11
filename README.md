a541a83a16a0365ebf7d280307f0135f79f0b90d
Phil Quinn <phil.quinn@livenation.co.uk>
2020-09-11 09:58:10 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Rename MMB Darwin Payslips
---
+++ rename_payslips.py
@@ -0,0 +1,65 @@
+import PyPDF2
+import re
+import datetime
+import os
+import glob
+import fnmatch
+
+def open_pdf(file_path):
+    pdfFileObj = open(file_path, 'rb')
+    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+
+    pages = pdfReader.numPages
+    pageObj = pdfReader.getPage(0)
+    text = pageObj.extractText()
+    return text
+
+def get_date(text):
+    match = re.search(r'Date (\d{2})\/(\d{2})\/(\d{4})', text)
+    if match:
+        day = match.group(1)
+        month = match.group(2)
+        year = match.group(3)
+
+    else:
+        raise ValueError("Date not found in text")
+    return datetime.datetime(int(year), int(month), int(day))
+
+def is_bonus(text):
+    match = re.search(r'Bonus', text)
+    if match:
+        return True
+    else:
+        return False
+
+
+def fix_file(input_filepath):
+    print(input_filepath)
+
+
+    text = open_pdf(input_filepath)
+    print(text)
+
+    payslip_date = get_date(text).strftime("%Y%m%d")
+    bonus_text = ""
+    if is_bonus(text):
+        bonus_text = " - bonus"
+
+    output_filepath = "data/Payslip - {}{}.pdf".format(payslip_date, bonus_text)
+    print("Input file path: {}".format(input_filepath))
+    print("Output file path: {}".format(output_filepath))
+
+
+    if input_filepath != output_filepath:
+        print("Renaming file")
+        os.rename(input_filepath, output_filepath)
+
+def main():
+    input_files = glob.glob("data/*.pdf")
+    input_files.extend(glob.glob("data/*.PDF"))
+    print(input_files)
+
+    for filepath in input_files:
+        fix_file(filepath)
+if __name__ == "__main__":
+    main()
